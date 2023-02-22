368f4f450047b3aef2d93b99d8fdb49c5adacfbd
Jordaan23 <josee.cornejo@gmail.com>
2023-02-22 17:12:19 -0300

    cambios hacia rutas del base.html

--- FarmaciaDigital/FarmaciaDigital/settings.py
+++ FarmaciaDigital/FarmaciaDigital/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&&5xw3cy!6t$(jfx+*pp1jaq37v6y3b3i#fv)-h(_$tm8!o*4d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
+#ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['devcode02.pythonanywhere.com']
-
 
 # Application definition
 
