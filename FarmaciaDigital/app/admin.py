67815c06f33718d66f3b7f3f4eda151547bca012
Jordaan23 <josee.cornejo@gmail.com>
2023-02-27 09:05:58 -0300

    cambios en el models

--- FarmaciaDigital/app/admin.py
+++ FarmaciaDigital/app/admin.py
@@ -1,15 +1,64 @@
 from django.contrib import admin
-from .models import Laboratorio, Principio, ViaAdminstracion, Medicamentos, Contacto
+from .models import Region, Provincia, Comuna, Tipo_usuario, Laboratorio, Principio, ViaAdminstracion, Medicamentos, Receta, Recordatorio, Familiar, Usuario, Contacto
 # Register your models here.
 
-#class MedicamentosAdmin(admin.ModelAdmin):
-#    list_display = ["laboratorio", "principio", "nombre", "dosis", "cantidad", "presentacion", "lote", "ingesta","fecha_vencimento"]
-#    search_fields = ["nombre"]
+#ADMIN DE REGION
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ["nombre_region"]
+    search_fields = ["nombre_region"]
+
+#ADMIN DE PROVINCIA
+class ProvinciaAdmin(admin.ModelAdmin):
+    list_display = ["nombre_provincia"]
+    search_fields = ["nombre_provincia"]
+
+#ADMIN DE COMUNA
+class ComunaAdmin(admin.ModelAdmin):
+    list_display = ["nombre_comuna"]
+    search_fields = ["nombre_comuna"]
+
+#ADMIN DE TIPO USUARIO
+class Tipo_usuarioAdmin(admin.ModelAdmin):
+    list_display = ["nombre_tipo_usuario"]
+    search_fields = ["nombre_tipo_usuario"]
     
+#ADMIN DE LABORATORIO
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ["nombre_laboratorio"]
+    search_fields = ["nombre_laboratorio"]
 
 
+#ADMIN DE PRINCIPIO ACTIVO
+class PrincipioAdmin(admin.ModelAdmin):
+    list_display = ["nombre_princio_activo"]
+    search_fields = ["nombre_princio_activo"]
+
+#ADMIN DE VIA ADMINISTRACION
+class ViaAdministracionAdmin(admin.ModelAdmin):
+    list_display = ["nombre_via_administracion"]
+    search_fields = ["nombre_via_administracion"]
+
+#ADMIN DE MEDICAMENTOS
+class MedicamentosAdmin(admin.ModelAdmin):
+    list_display = ["laboratorio","nombre_princio_activo","nombre_comercial", "dosis", "cantidad", "presentacion","lote", "ingesta", "fecha_vencimento"]
+    search_fields = ["nombre_via_administracion"]
+    list_editable = ["laboratorio","nombre_princio_activo","nombre_comercial", "dosis", "cantidad", "presentacion","lote", "ingesta", "fecha_vencimento"]
+
+#ADMIN DE CONTACTO
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "email", "tipo_consulta", "mensaje"]
+    search_fields = ["email"]
+
+admin.site.register(Region)
+admin.site.register(Provincia)
+admin.site.register(Comuna)
+admin.site.register(Tipo_usuario)
 admin.site.register(Laboratorio)
 admin.site.register(Principio)
 admin.site.register(ViaAdminstracion)
 admin.site.register(Medicamentos)
 admin.site.register(Contacto)
+admin.site.register(Receta)
+admin.site.register(Recordatorio)
+admin.site.register(Familiar)
+admin.site.register(Usuario)
--- FarmaciaDigital/app/models.py
+++ FarmaciaDigital/app/models.py
@@ -1,38 +1,59 @@
 from django.db import models
 
+#TABLA DE REGION
+class Region(models.Model):
+    nombre_region = models.CharField(max_length=45)
+    def __str__(self):
+        return self.nombre_region
+    
+#TABLA DE PROVINCIA
+class Provincia(models.Model):
+    nombre_provincia = models.CharField(max_length=100)
+    nombre_region =models.ForeignKey(Region, on_delete=models.PROTECT )
+    def __str__(self):
+        return self.nombre_provincia    
+    
+#TABLA DE COMUNA
+class Comuna(models.Model):
+    nombre_comuna = models.CharField(max_length=100)
+    nombre_provincia = models.ForeignKey(Provincia, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.nombre_comuna
+
+#TABLA TIPO DE USURIO
+class Tipo_usuario(models.Model):
+    nombre_tipo_usuario =models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre_tipo_usuario    
+
+
 #TABLA LABORATORIO
 class Laboratorio(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre_laboratorio = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre_laboratorio
 
 #TABLA DE PRINCIPIO ACTIVO
 class Principio(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre_princio_activo = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nombre        
+        return self.nombre_princio_activo        
 
 #TABLA DE ADMINISTRACION MEDICAMENTO
 class ViaAdminstracion(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre_via_administracion = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre_via_administracion
 
-#MARCA DEL MEDICAMENTO
-#class MarcaMedicamento(models.Model):
-#    nombre = models.CharField(max_length=50)
-#
-#    def __str__(self):
-#        return self.nombre
 
 #TABLA DE MEDICAMENTOS
 class Medicamentos(models.Model):
     laboratorio = models.ForeignKey(Laboratorio, on_delete=models.PROTECT)
-    principio = models.ForeignKey(Principio, on_delete=models.PROTECT)
-    nombre = models.CharField(max_length=100)
+    nombre_princio_activo = models.ForeignKey(Principio, on_delete=models.PROTECT)
+    nombre_comercial = models.CharField(max_length=100)
     dosis = models.CharField(max_length=50)
     cantidad =models.IntegerField()
     presentacion =models.CharField(max_length=100)
@@ -42,7 +63,67 @@ class Medicamentos(models.Model):
     #imagen = models.ImageField(upload_to="medicamentos", null=True)
 
     def __str__(self):
-        return self.nombre
+        return self.nombre_princio_activo
+    
+#TABLA DE RECETA
+class Receta(models.Model):
+    fecha_receta = models.DateField()
+    horario_recordatorio = models.DateTimeField()
+    descripcion_receta = models.CharField(max_length=500)
+    def __date__ (self):
+        return self.descripcion_receta
+    
+#TABLA DE RECORDATORIO
+class Recordatorio(models.Model):
+    nombre_recordatorio = models.CharField(max_length=100)
+    hora_recordatorio = models.DateTimeField()
+    descripcion_receta = models.ForeignKey(Receta, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.nombre_recordatorio
+
+    
+#TABLA DE FAMILIAR
+class Familiar(models.Model):
+    num_run_familiar = models.CharField(max_length=12)
+    nombre_familiar = models.CharField(max_length=100)
+    apellido_familiar = models.CharField(max_length=100)
+    direccion_familiar = models.CharField(max_length=100)
+    email_familiar = models.EmailField()
+    telefono_familiar = models.IntegerField()
+    whatsapp_familiar = models.IntegerField()
+    celular_familiar = models.IntegerField()
+    telegram_familiar = models.IntegerField()
+    nombre_comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
+    nombre_provincia = models.ForeignKey(Provincia, on_delete=models.PROTECT)
+    def __str__ (self):
+        return self.nombre_familiar
+
+
+#TABLA USUARIO
+class Usuario (models.Model):
+    num_run_usuario = models.CharField(max_length=12)
+    nombres_usuario = models.CharField(max_length=100)
+    apellido_paterno_usuario = models.CharField(max_length=100)
+    appelido_materno_usuario = models.CharField(max_length=100)
+    dirreccion_usuario = models.CharField(max_length=150)
+    email_usuario = models.EmailField()
+    telefono_usuario = models.IntegerField()
+    celular_usuario = models.IntegerField()
+    whatsapp_usuario = models.IntegerField()
+    nombre_comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
+    nombre_familiar = models.ForeignKey(Familiar, on_delete=models.PROTECT)
+    nombre_comercial = models.ForeignKey(Medicamentos, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.nombres_usuario
+
+#MARCA DEL MEDICAMENTO
+#class MarcaMedicamento(models.Model):
+#    nombre = models.CharField(max_length=50)
+#
+#    def __str__(self):
+#        return self.nombre
+
+
 
 #OPCIONES DE CONSULTA EN CONTACTO
 opciones_consulta = [
@@ -61,3 +142,8 @@ class Contacto(models.Model):
     mensaje = models.TextField()
     def __str__(self):
         return self.nombre
+
+
+
+
+
