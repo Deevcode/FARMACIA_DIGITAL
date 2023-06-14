e2691ebcef860abdc7b70778d078610752cb5f54
JoseCarlos <josee.cornejo@gmail.com>
2023-06-14 12:28:38 -0400

    cambios en el models.py

--- FarmaciaDigital/app/models.py
+++ FarmaciaDigital/app/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 #TABLA DE REGION
 class Region(models.Model):
     id_region = models.AutoField(primary_key=True)
-    nombre_region = models.CharField(max_length=45)
+    nombre_region = models.CharField(max_length=45,null=False, blank=False)
 
     def __str__(self):
         return self.nombre_region
@@ -17,7 +17,7 @@ class Region(models.Model):
 #TABLA DE PROVINCIA
 class Provincia(models.Model):
     id_provincia = models.AutoField(primary_key=True)
-    nombre_provincia = models.CharField(max_length=100)
+    nombre_provincia = models.CharField(max_length=100,null=False, blank=False)
     id_region =models.ForeignKey(Region, on_delete=models.PROTECT)
 
     def __str__(self):
@@ -28,7 +28,7 @@ class Provincia(models.Model):
 #TABLA DE COMUNA
 class Comuna(models.Model):
     id_comuna = models.AutoField(primary_key=True)
-    nombre_comuna = models.CharField(max_length=100)
+    nombre_comuna = models.CharField(max_length=100,null=False, blank=False)
     id_provincia = models.ForeignKey(Provincia, on_delete=models.PROTECT)
 
     def __str__(self):
@@ -39,7 +39,7 @@ class Comuna(models.Model):
 #TABLA LABORATORIO
 class Laboratorio(models.Model):
     id_laboratorio = models.AutoField(primary_key=True)
-    nombre_laboratorio = models.CharField(max_length=100)
+    nombre_laboratorio = models.CharField(max_length=100,null=False, blank=False)
 
     def __str__(self):
         return self.nombre_laboratorio
@@ -49,7 +49,7 @@ class Laboratorio(models.Model):
 #TABLA DE PRINCIPIO ACTIVO
 class PrincipioActivo(models.Model):
     id_principio_activo= models.AutoField(primary_key=True)
-    nombre_princio_activo = models.CharField(max_length=100)
+    nombre_princio_activo = models.CharField(max_length=100,null=False, blank=False)
 
     def __str__(self):
         return self.nombre_princio_activo        
@@ -59,7 +59,7 @@ class PrincipioActivo(models.Model):
 #TABLA DE ADMINISTRACION MEDICAMENTO
 class ViaAdminstracion(models.Model):
     id_via_administracion = models.AutoField(primary_key=True)
-    nombre_via_administracion = models.CharField(max_length=100)
+    nombre_via_administracion = models.CharField(max_length=100,null=False, blank=False)
 
     def __str__(self):
         return self.nombre_via_administracion
@@ -69,7 +69,7 @@ class ViaAdminstracion(models.Model):
 #TABLA DE FARMACIA
 class Farmacia(models.Model):
     id_farmacia = models.AutoField(primary_key=True)
-    marca_farmacia = models.CharField(max_length=200)
+    marca_farmacia = models.CharField(max_length=200,null=False, blank=False)
 
     def __str__(self):
         return self.marca_farmacia
@@ -81,10 +81,10 @@ class Medicamentos(models.Model):
     id_medicamento = models.AutoField(primary_key=True)
     id_laboratorio = models.ForeignKey(Laboratorio, on_delete=models.PROTECT)
     id_principio_activo = models.ForeignKey(PrincipioActivo, on_delete=models.PROTECT)
-    nombre_comercial = models.CharField(max_length=100)
-    gramaje = models.CharField(max_length=50)
-    cantidad_stock =models.IntegerField()
-    presentacion_medicamento =models.CharField(max_length=100)
+    nombre_comercial = models.CharField(max_length=100,null=False, blank=False)
+    gramaje = models.CharField(max_length=50,null=False, blank=False)
+    cantidad_stock =models.IntegerField(null=False, blank=False)
+    presentacion_medicamento =models.CharField(max_length=100,null=False, blank=False)
     id_via_administracion = models.ForeignKey(ViaAdminstracion, on_delete=models.PROTECT)
     #imagen = models.ImageField(upload_to="medicamentos", null=True)
 
@@ -98,9 +98,9 @@ class MedicamentosDescuento(models.Model):
     id_descuento = models.AutoField(primary_key=True)
     medicamento_desc = models.ForeignKey(Medicamentos, on_delete=models.PROTECT)
     id_farmacia = models.ForeignKey(Farmacia, on_delete=models.PROTECT)
-    fecha_inicio_descuento = models.DateField()
-    fecha_termino_descuento = models.DateField()
-    descuento_porcentaje = models.IntegerField()
+    fecha_inicio_descuento = models.DateField(null=False)
+    fecha_termino_descuento = models.DateField(null=False)
+    descuento_porcentaje = models.IntegerField(null=False)
 
     def __str__(self):
         return str(self.medicamento_desc)+" con un %"+str(self.descuento_porcentaje)
@@ -112,7 +112,7 @@ class MedicamentoFichaTecnica(models.Model):
     id_ficha_medicamento = models.AutoField(primary_key=True)
     laboratorio_ficha = models.ForeignKey(Laboratorio, on_delete=models.PROTECT)
     nombre_comercial = models.ForeignKey(Medicamentos, on_delete=models.PROTECT)
-    url_ficha = models.CharField(max_length=700)
+    url_ficha = models.CharField(max_length=700,null=False, blank=False)
 
     def __str__(self):
         return self.url_ficha
@@ -122,7 +122,7 @@ class MedicamentoFichaTecnica(models.Model):
 #TABLA TIPO DE USURIO
 class Tipo_usuario(models.Model):
     id_TipoUsuario = models.AutoField(primary_key=True)
-    nombre_tipo_usuario =models.CharField(max_length=100)
+    nombre_tipo_usuario =models.CharField(max_length=100,null=False, blank=False)
     def __str__(self):
         return self.nombre_tipo_usuario  
 
@@ -131,7 +131,7 @@ class Tipo_usuario(models.Model):
 #TABLA USUARIO
 class Usuario(AbstractUser):
 
-    rut_usuario = models.CharField(max_length=12)
+    rut_usuario = models.CharField(max_length=12,null=False, blank=False)
     tipo_usuario = models.ForeignKey(Tipo_usuario, on_delete=models.PROTECT,null=True)
 
     def __str__(self):
@@ -145,9 +145,9 @@ class Usuario(AbstractUser):
 class UsuarioFicha (models.Model):
     id_usuario = models.AutoField(primary_key=True)
     identificacion_usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL,null=True)
-    dirreccion_usuario = models.CharField(max_length=150)
-    telefono_usuario = models.IntegerField()
-    celular_usuario = models.IntegerField()
+    dirreccion_usuario = models.CharField(max_length=150,null=False, blank=False)
+    telefono_usuario = models.IntegerField(null=False)
+    celular_usuario = models.IntegerField(null=False)
     id_comuna = models.ForeignKey(Comuna, on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return str(self.identificacion_usuario)+" "+str(self.telefono_usuario)
@@ -166,11 +166,11 @@ opciones_fraccionamiento = [
 #TABLA DE USUARIO RECETA
 class PacienteReceta(models.Model):
     id_receta_usuario = models.AutoField(primary_key=True)
-    fecha_receta = models.DateField()
+    fecha_receta = models.DateField(null=False)
     nombres_paciente = models.ForeignKey(Usuario, on_delete=models.SET_NULL,null=True,related_name="RRPaciente",limit_choices_to=Q(tipo_usuario_id=2))
     nombre_comercial = models.ForeignKey(Medicamentos, on_delete=models.SET_NULL,null=True)
-    tiempo_tratamiento_dias = models.CharField(max_length=100)
-    frecuencia_dosis_diaria = models.CharField(max_length=100)
+    tiempo_tratamiento_dias = models.CharField(max_length=100,null=False, blank=False)
+    frecuencia_dosis_diaria = models.CharField(max_length=100,null=False, blank=False)
     horario_1 = models.TimeField(null=True)
     fracionamiento_1 = models.IntegerField(choices=opciones_fraccionamiento, null=True)
     horario_2 = models.TimeField(blank=True ,null=True)
@@ -195,7 +195,7 @@ class PacienteFamiliar(models.Model):
     id_usuario_familiar = models.AutoField(primary_key=True)
     identificacion_usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True,related_name="PacienteUser",limit_choices_to=Q(tipo_usuario_id=2))
     identificacion_familiar = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True,related_name="PacienteFamiliar",limit_choices_to=Q(tipo_usuario_id=3))
-    parentesco = models.CharField(max_length=100)
+    parentesco = models.CharField(max_length=100,null=False, blank=False)
 
     def __str__ (self):
         return  str(self.identificacion_familiar)+": "+str(self.parentesco)
@@ -206,9 +206,9 @@ class PacienteFamiliar(models.Model):
 class CESFAM(models.Model):
     id_cesfam = models.AutoField(primary_key=True)
     identificacion_cesfam = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, related_name="CESFAM",limit_choices_to=Q(tipo_usuario_id=5))
-    email_CESFAM = models.CharField(max_length=200)
-    telefono_CESFAM = models.CharField(max_length=200)
-    direccion_CESFAM = models.CharField(max_length=200)
+    email_CESFAM = models.CharField(max_length=200,null=False, blank=False)
+    telefono_CESFAM = models.CharField(max_length=200,null=False, blank=False)
+    direccion_CESFAM = models.CharField(max_length=200,null=False, blank=False)
     nombre_comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
 
     def __str__ (self):
@@ -220,7 +220,7 @@ class CESFAM(models.Model):
 class QuimicoFarmaceuticoEncargado(models.Model):
     id_quimico_farmaceutio = models.AutoField(primary_key=True)
     identificacion_QF = models.ForeignKey(Usuario, on_delete=models.SET_NULL,null=True,related_name="QUIMICO",limit_choices_to=Q(tipo_usuario_id=4))
-    registro_sanitario_QF = models.CharField(max_length=200)
+    registro_sanitario_QF = models.CharField(max_length=200,null=False, blank=False)
 
     def __str__(self):
         return str(self.identificacion_QF)+" "+str(self.registro_sanitario_QF)
@@ -233,9 +233,9 @@ class FarmaciaSucursal(models.Model):
     nombre_farmacia = models.ForeignKey(Farmacia, on_delete=models.PROTECT)
     id_quimico_farmaceutio = models.ForeignKey(QuimicoFarmaceuticoEncargado ,on_delete= models.PROTECT)
     nombre_comuna = models.ForeignKey(Comuna, on_delete=models.PROTECT)
-    direccion_sucursal = models.CharField(max_length=200)
-    telefono_sucursal = models.IntegerField()
-    email = models.EmailField()
+    direccion_sucursal = models.CharField(max_length=200,null=False, blank=False)
+    telefono_sucursal = models.IntegerField(null=False)
+    email = models.EmailField(null=False)
 
     def __str__(self):
         return str(self.nombre_farmacia)+" "+str(self.direccion_sucursal)+" "+str(self.nombre_comuna)+" tel:"+str(self.telefono_sucursal)
@@ -274,7 +274,7 @@ class PacienteFichaClinica(models.Model):
     identificacion_paciente = models.ForeignKey(Usuario, on_delete=models.PROTECT,related_name="FCPaciente",limit_choices_to=Q(tipo_usuario_id=2))
     tipo_diabetes = models.IntegerField(choices=opciones_diabetes, null=True)
     tipo_hipertension = models.IntegerField(choices=opciones_hipertension, null=True)
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(null=False)
 
     def __str__ (self):
         return str(self.identificacion_paciente)+ ": Diabetes:"+str(self.tipo_diabetes)+", Hipertension:"+str(self.tipo_hipertension)
@@ -292,11 +292,11 @@ opciones_consulta = [
 
 #TABLA DE CONTACTO
 class Contacto(models.Model):
-    repetir_email = models.EmailField()
-    nombre = models.CharField(max_length=100)
+    repetir_email = models.EmailField(null=False, blank=False)
+    nombre = models.CharField(max_length=100,null=False, blank=False)
     tipo_consulta = models.IntegerField(choices=opciones_consulta, null=True)
-    telefono = models.IntegerField()
-    mensaje = models.TextField()
+    telefono = models.IntegerField(null=False, blank=False)
+    mensaje = models.TextField(null=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
